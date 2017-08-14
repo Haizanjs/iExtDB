daf65bfc4732d5326d59b53c19e5b702040ac9d0
Heisen <jackmichaelsimons59@gmail.com>
2017-08-14 22:29:30 +0100

    Update ReadMe.

--- iExtDB/config.cpp
+++ iExtDB/config.cpp
@@ -2,7 +2,7 @@ class cfgPatches {
 	class iExtDB {
 		name = "iExtDB";
 		author = "";
-		url = "";
+		url = "https://github.com/jackmsimo/iExtDB";
 		requiredVersion = 1.74;
 		requiredAddons[] = {"extDB3"};
 	};
