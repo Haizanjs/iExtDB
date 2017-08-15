c0691a3ae45116d688bdd4a81c94a44fbea39304
Jack (Heisen) <jackmichaelsimons59@gmail.com>
2017-08-15 11:49:47 +0100

    aSync, updated Credits

--- iExtDB/iExtDB_Functions.hpp
+++ iExtDB/iExtDB_Functions.hpp
@@ -3,6 +3,12 @@ class cfgFunctions {
 		class iExtDB_Main {
 			file "\iExtDB\iExtDB_Functions\Main";
 			class establishConnection {};
+			class databaseASync {};
+			class databaseQuery {};
+		};
+		class iExtDB_Calls {
+			file "\iExtDB\iExtDB_Functions\Calls";
+			class queryRequest {};
 		};
 		class iExtDB_Misc {
 			file "\iExtDB\iExtDB_Functions\Misc";
